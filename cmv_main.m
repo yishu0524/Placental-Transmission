7d6984843bad1fa293c08263642172e3ccd13943
Yishu Gong <55518142+yishu0524@users.noreply.github.com>
2021-04-01 09:49:14 -0400

    Add files via upload

--- cmv_main.m
+++ cmv_main.m
@@ -1,6 +1,6 @@
 %viral load on mother side
 initial_values = [1e-4, 0, 0,4e2,0]; % (V, E, RI, RS, RL) 
- [t_m,y] = ode15s(@mother,[0 280],initial_values); 
+ [t_m,y] = ode15s(@mother,[0 166],initial_values); 
 % xlabel('Time t');
 % ylabel('Solution y');
 % set(gca, 'YScale', 'log')
@@ -16,36 +16,43 @@ plot(t_m,y(:,1)*1e3,'sb',t2,V(t2),'*r')
 
 %%
 l=2; %width of the placenta
-T=280;
+T=166;
 D=0.0069;
-c2=mean(y_m);
 nx=2000;
 nt=1000;
 x = linspace(0,l,nx); %2–2.5 cm (0.8–1 inch) in thickness, 
 t = linspace(0,T,nt); %if we run simulation for 20 days
 m = 0;
-s1=t_m;s2=y_m;p=0.5/D;
+s1=t_m;s2=y_m;p=0.15/D;
 
 
 options=odeset('RelTol',1e-4,'AbsTol',1e-4,'NormControl','off','InitialStep',1e-7) 
 sol = pdepe(0,@unsatpde,@unsatic,@unsatbc,x,t,options,s1,s2,p,D); 
 u= sol(:,:,1);
 figure (2)
-plot(x,u(end,:)) 
+plot(x,u(end,:))
+%%
+placenta_growth=10000./(1+ exp(-(0.05*(t-80))));
 [ux,ut]=gradient(u,l/nx,T/nt);
-virus_numeric=trapz(ux(:,end))*T/nt*D*28858
+
+virus_numeric=trapz(ux(:,1) .*transpose(placenta_growth) )*T/nt*D
 %%
 figure (3)
-surf(x,t,u)
+surf(x,t,u,'FaceAlpha',0.5,'EdgeColor','none')
 title('Numerical solution')
 xlabel('Distance x')
 ylabel('Time t')
+ylim([0,166])
+zlabel('CMV DNA Viral Load copies')
+ax=gca;
+ax.YDir="reverse"
 %%
 
+%%
 function [c,f,s] = unsatpde(x,t,u,DuDx,s1,s2,p,D) 
 c =1/D;% 1/(1.8*10^-3) ; %inverse of the diffusion coefficent
 f = DuDx;
-s =-p*u; %death of the cmv %death rate is 1, 2 virus day per day
+s =-p *u; %death of the cmv %death rate is 1, 2 virus day per day
 end 
 % ------------------------------------------------------------------------- 
 function u0 = unsatic(x,s1,s2,p,D) 
@@ -53,9 +60,8 @@ u0 = 0; %initial is 0
 end 
 % ------------------------------------------------------------------------- 
 function [pl,ql,pr,qr] = unsatbc(xl,ul,xr,ur,t,s1,s2,p,D) 
-pl = ul-interp1(s1,s2,t,'linear','extrap'); %u(0,t)=1
+pl = ul; %u(l,t)=1
 ql = 0; 
-pr = ur; %u(L,t)=0
+pr = ur-interp1(s1,s2,t,'linear','extrap'); %u(r,t)=0
 qr = 0;
 end 
-%%
--- expdata.m
+++ expdata.m
@@ -11,11 +11,11 @@ Monkey3=Monkey3(:, {'days', 'viralLoad'})
 Monkey1=Monkey1(~any(ismissing(Monkey1),2),:);
 Monkey2=Monkey2(~any(ismissing(Monkey1),2),:);
 Monkey3=Monkey3(~any(ismissing(Monkey1),2),:);
-T_end =table(166,0,'VariableNames',{'days', 'viralLoad'})
+T_end =table(166,1,'VariableNames',{'days', 'viralLoad'})
 Monkey1=[Monkey1;T_end]
 Monkey2=[Monkey2;T_end]
 %%
-T=28;
+T=166;
 l=2; %width of the placenta
 D=0.0069;
 nx=2000;
@@ -36,43 +36,45 @@ semilogy(Monkey2.days,Monkey2.viralLoad,'o-')
 hold on
 semilogy(Monkey3.days,Monkey3.viralLoad,'o-')
 hold off
-title('Viral Load Data from mother Macaque')
+title('Viral Load Data from mother Macaques')
 xlabel('Time t (days)')
-ylabel('Viral Load (Log Scale)')
+ylabel('CMV DNA Viral Load copies/ml')
 legend('40690','34795','45010')
 %%
 %plot the 1d interpolation 
 
 figure(1)
-plot(t,Monkey3_VL) 
+plot(t,Monkey2_VL) 
 hold on
-plot(Monkey3.days,Monkey3.viralLoad,'o')
+plot(Monkey2.days,Monkey2.viralLoad,'o')
+
 hold off
-title('Viral Load Data from mother Macaque')
+title('Viral Load Data from mother Macaque 34795')
 xlabel('Time t (days)')
-ylabel('Viral Load')
-%legend('Interpolation (pchip)','Data')
+ylabel('CMV DNA Viral Load copies/ml')
+%legend('40690','34795','45010')
 %%
 s1=t;
-s2=Monkey3_VL;
-p=0.4/D;
+s2=Monkey1_VL;
+p=2/D;
 
 
 options=odeset('RelTol',1e-4,'AbsTol',1e-4,'NormControl','off','InitialStep',1e-7) 
 sol = pdepe(0,@unsatpde,@unsatic,@unsatbc,x,t,options,s1,s2,p,D); 
 u= sol(:,:,1);
 %%
+placenta_growth=10000./(1+ exp(-(0.05*(t-80))));
+%%
 [ux,ut]=gradient(u,l/nx,T/nt);
 
 virus_numeric=trapz(ux(:,1) .*transpose(placenta_growth) )*T/nt*D
-%%
-placenta_growth=10000./(1+ exp(-(0.05*(t-80))));
+
 %%
 figure(2)
 plot(t,placenta_growth)
 title('Placental growth over pregancy')
 xlabel('Time t (days)')
-ylabel('Placental Surface Area')
+ylabel('Placental Surface Area (mm^2)')
 legend('Placental Growth Function')
 %%
 Q = ux(:,1).* D.*transpose(placenta_growth);
@@ -85,23 +87,26 @@ plot(t,Q,'b')
 %plot(t, ux(:,1).* D*avg,'g')
 %hold off
 ylim([0 inf])
-title('Viral flux on the infant side over time')
+title('Viral flux on the infant of mother Macaque 40690 over time')
 xlabel('Time t (days)')
-ylabel('Viral flux on the infant side')
+ylabel('CMV DNA Viral Load copies/(μl*mm*day)')
 %legend('Growth Function','Average Size')
 %%
 figure (3)
 surf(x,t,u,'FaceAlpha',0.5,'EdgeColor','none')
-hold on
-%surf(x,t,u)
+hold on 
+scatter3([2,2,2,2,2,2,2],Monkey2.days,Monkey2.viralLoad,'or','filled')
 xlabel('Distance x (mm)')
 ylabel('Time t (days)')
+ylim([0,166])
+zlabel('CMV DNA Viral Load copies/μl')
 ax=gca;
 ax.YDir="reverse"
 hold off
-title('Placental Tranport of CMV')
+title('Placental Tranport of CMV for mother Macaque 34795')
+
 %%
-temp=[transpose(t),Q]
+t=transpose(t)
 %%
 function [c,f,s] = unsatpde(x,t,u,DuDx,s1,s2,p,D) 
 c =1/D;% 1/(1.8*10^-3) ; %inverse of the diffusion coefficent
